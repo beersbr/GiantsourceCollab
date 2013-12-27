86b1f400b8b362b1cf831b9d263951c250fc2bab
Keith Stewart <kwstewart@gmail.com>
2013-12-26 22:27:27 -0500

    removed console and cleaned up line breaks

--- tactics/mapRender.js
+++ tactics/mapRender.js
@@ -6,15 +6,12 @@ function renderTiles(mapObj) {
   
   // Loop through each row
   for(var r in mapObj) {
-    console.log("row: "+r);
     var row = mapObj[r];
     // Loop through each column
     for(var c in row) {      
       var tile = row[c];
-      
       // Set the class for tile  
       var tileClass = "";
-      
       if(r == 0) tileClass += " top";      // First row, top of map
       if(c == row.length - 1) tileClass += " right";    // Last column, right of map
       if(r == mapObj.length - 1) tileClass += " bottom";   // Last row, bottom of map
@@ -24,7 +21,6 @@ function renderTiles(mapObj) {
       $("#map").append("<div class='"+tileClass+"'><img src='"+tile.image+"'/></div>");     
     }
   }
-  
 }
 
 
