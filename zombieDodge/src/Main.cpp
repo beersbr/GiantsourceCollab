4f6ba955c20e8b754df5d2bd3ceab6cd612d3947
Matt Jacobs <theunlisted@gmail.com>
2014-01-09 14:53:32 -0800

    added game loop and player class

--- zombieDodge/src/Main.cpp
+++ zombieDodge/src/Main.cpp
@@ -1,46 +1,327 @@
+//
+//  main.cpp
+//  Test Project
+//
+//  Created by Matt Jacobs on 1/7/14.
+//  Copyright (c) 2014 Matt Jacobs. All rights reserved.
+//
+
 #include <iostream>
 #include "Vector.h"
-#include <SDL2/SDL.h>
 
-// This was taken from a tutorial website as a test. make sure it runs :D
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include "config.h"
+#include <stack>
+#include <stdio.h>
+#include "Player.h"
+#include <string>
+
+struct StateStruct
+{
+    void (*StatePointer)();
+};
+
+// Global data //
+std::stack<StateStruct> gameStates;
+
+//Starts up SDL and creates window
+bool init();
+
+//Updates the main loop
+void update();
+
+//Frees media and shuts down SDL
+void shutdown();
+
+//The window we'll be rendering to
+SDL_Window* gameWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gameSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gameBackground = NULL;
+
+SDL_Event		   gameEvent;
+int				   gameTimer;
+int currentGameState = 0;
+int currentLevel = 0;
+
+//Loads Optimized Surface
+SDL_Surface* loadOptSurface( std::string path );
+
+
+
+SDL_Surface* loadOptSurface( std::string path )
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gameSurface->format, NULL );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return optimizedSurface;
+}
 
 namespace game
 {
 
+
+
+        bool loadScreen(int screenId)
+            {
+                //Loading success flag
+                bool success = true;
+
+             switch(screenId)
+
+              {
+                    case 0:
+                         gameBackground = loadOptSurface( "welcome.jpg" );
+                         break;
+
+              }
+
+
+              if(gameBackground == NULL )
+                 {
+                     printf( "Failed to load  image!\n" );
+                     success = false;
+                 }   else {
+
+                     success = true;
+                 }
+
+                 return success;
+        };
+
+
+    bool loadStateResources(int screenId)
+    {
+        //Loading success flag
+        bool success = true;
+
+        switch(screenId)
+
+        {
+            case 0:
+
+                break;
+
+        }
+
+
+        return success;
+    };
+
+
+    void menuInput() {
+
+        switch( gameEvent.key.keysym.sym ) {
+
+            case SDLK_UP:
+                printf( "KEY UP\n" );
+                currentGameState = 1;
+                break;
+            case SDLK_DOWN:
+                printf( "KEY DOWN\n" );
+                break;
+
+        }
+
+    };
+
+    void gameInput() {
+
+        switch( gameEvent.key.keysym.sym ) {
+
+            case SDLK_UP:
+                printf( "KEY UP\n" );
+                break;
+            case SDLK_DOWN:
+                printf( "KEY DOWN\n" );
+                break;
+
+        }
+
+    };
+
     class Entity
     {
+
+
+
     public:
         Vector pos;
+
     };
+
+
+
 
 }
 
-int main(int argc, char * arg[])
+bool init()
 {
-    // init SDL
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    //Initialization flag
+    bool success = true;
+
+    gameTimer = SDL_GetTicks();
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
     {
-        std::cout << "ERROR SDL_Init" << std::endl;
-        
-        return -1;
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gameWindow = SDL_CreateWindow(
+                WINDOW_CAPTION,             // window title
+                SDL_WINDOWPOS_CENTERED,     // x position, centered
+                SDL_WINDOWPOS_CENTERED,     // y position, centered
+                WINDOW_WIDTH,                        // width, in pixels
+                WINDOW_HEIGHT,                        // height, in pixels
+                SDL_WINDOW_OPENGL           // flags
+        );
+        if( gameWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Initialize JPG loading
+            int imgFlags = IMG_INIT_JPG;
+            if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            {
+                printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Get window surface
+                gameSurface = SDL_GetWindowSurface( gameWindow );
+            }
+        }
     }
 
-    // create a window
-    SDL_Window * window = SDL_CreateWindow(
-                                           "SDL 2 window",             // window title
-                                           SDL_WINDOWPOS_CENTERED,     // x position, centered
-                                           SDL_WINDOWPOS_CENTERED,     // y position, centered
-                                           640,                        // width, in pixels
-                                           480,                        // height, in pixels
-                                           SDL_WINDOW_OPENGL           // flags
-                                           );
-    
-    // wait 5 secs
-    SDL_Delay(5000);
-    
-    // clean up
-    SDL_DestroyWindow(window);
+    return success;
+}
+
+void update(){
+
+    if ( (SDL_GetTicks() - gameTimer) >= FRAME_RATE )
+    {
+
+        //Apply the image
+        SDL_BlitSurface( gameBackground, NULL, gameSurface, NULL );
+
+        //Update the surface
+        SDL_UpdateWindowSurface( gameWindow );
+
+        switch(currentGameState) {
+
+
+            case -1:
+                //Load the Welcome screen
+                game::loadStateResources(currentGameState);
+                break;
+
+            case 0:
+                //Load the Welcome screen
+                game::loadScreen(currentGameState);
+                game::menuInput();
+                break;
+
+            case 1:
+                //Load the Welcome screen
+                Player::update();
+                game::gameInput();
+                break;
+
+
+
+        }
+
+
+        gameTimer = SDL_GetTicks();
+    }
+
+
+
+}
+
+void shutdown() {
+
+    SDL_FreeSurface( gameBackground );
+    gameBackground = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow( gameWindow );
+    gameWindow = NULL;
+
+    // Tell SDL to shutdown and free any resources it was using. //
     SDL_Quit();
-    
+
+}
+
+int main( int argc, char* args[] )
+{
+
+
+    //Start up SDL and create window
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+
+            //Main loop flag
+            bool quit = false;
+
+
+            //While application is running
+            while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &gameEvent ) != 0 )
+                {
+                    //User requests quit
+                    if( gameEvent.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    } //User presses a key
+                    else {
+
+                    }
+                }
+
+                update();
+        }
+    }
+
+    //Free resources and close SDL
+    shutdown();
+
     return 0;
 }
---
+++ zombieDodge/src/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Window related defines //
+#define WINDOW_WIDTH   800
+#define WINDOW_HEIGHT  600
+#define WINDOW_CAPTION "Stupid Zombies"
+
+// Game related defines //
+#define FRAMES_PER_SECOND 30
+#define FRAME_RATE        1000/FRAMES_PER_SECOND
